aa919fe25f98b4cb79a27c224dce952d537caa63
Marlon Vargas <168598816+pwaved@users.noreply.github.com>
2025-01-19 22:36:26 -0300

    Update script.js
    implementacao da funcionalidade da checkbox e lógica nas datas

--- BetterThxn/script.js
+++ BetterThxn/script.js
@@ -33,3 +33,64 @@ accordions.forEach((accordion) => {
     }
   });
 });
+
+
+const returnCheckbox = document.getElementById('local-devolve');
+const localInput = document.getElementById('local');
+// evento da mudança do checkbox
+returnCheckbox.addEventListener('change', () => {
+  let existingInput = document.getElementById('local-devolucao');
+  let existingLabel = document.getElementById('label-local-devolucao'); 
+
+  if (returnCheckbox.checked) {
+    //evitar duplicação de campos
+    if (!existingInput && !existingLabel) {
+      const returnInput = document.createElement('input');
+      const returnLabel = document.createElement('label');
+      returnLabel.id = 'label-local-devolucao';
+      returnLabel.setAttribute('for', 'local-devolucao');
+      returnLabel.textContent = 'Local de Devolução:';
+      returnInput.type = 'text';
+      returnInput.name = 'local-devolucao';
+      returnInput.id = 'local-devolucao';
+      returnInput.placeholder = 'Digite o local de devolução';
+
+      // insere o input do local de devolucao apos o local de retirada
+      
+      localInput.insertAdjacentElement('afterend', returnInput);
+      localInput.insertAdjacentElement('afterend', returnLabel);
+    }
+    // remove os campos
+  } else {
+    if (existingInput && existingLabel) {
+      existingInput.remove();
+      existingLabel.remove();
+    }
+  }
+});
+
+const dataInicio = document.getElementById("data-inicio");
+const dataFim = document.getElementById("data-fim");
+
+function validarDataRetirada() {
+  const hoje = new Date();
+  const dataRetirada = new Date(dataInicio.value); 
+
+  if (dataRetirada < hoje.setHours(0,0,0,0)) {
+    alert("A data de retirada não pode ser anterior a data de hoje!");
+    dataInicio.value = ''; // limpa o campo 
+  }
+}
+
+function validarDataDevolucao() { 
+  const dataRetirada = new Date(dataInicio.value);
+  const dataDevolucao = new Date(dataFim.value);
+
+  if(dataDevolucao < dataRetirada) {
+    alert("A data de devolução não pode ser menor que a data de retirada!")
+    dataFim.value = ''; // limpa o campo  
+  }
+}
+
+dataInicio.addEventListener('change', validarDataRetirada); 
+dataFim.addEventListener('change', validarDataDevolucao);
